9f8886a1cf22bb64da65d3f391801d8ef38c87bd
Twin_N <thetwin11100@gmail.com>
2019-02-12 04:51:54 +0600

    added the skills section

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
 particlesJS.load('particles-js', 'settings/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+
+document.querySelectorAll('.our-services .skill').forEach(function(currentNode){
+  console.log(currentNode);
+  currentNode.addEventListener('click', function(){
+    currentNode.style.height += window.getComputedStyle(currentNode.querySelector('.skill-list')).height
+  })
+})
+
+
+//(left*distance)/perspective
+// const perspective = parseInt(window.getComputedStyle(document.querySelector('.home-container')).perspective);    //perspective of the main mother element
+// const mm_translateZ = new WebKitCSSMatrix(window.getComputedStyle(document.querySelector('.mm .parallax-img')).webkitTransform).m43;  //translateZ value of mm image
+// document.querySelectorAll('.mm').forEach(function(currentNode, index){
+//   let mm_left = parseInt(window.getComputedStyle(currentNode).getPropertyValue('left'));  //X coordinate fo the current mm
+//   console.log( ( mm_left * Math.abs(mm_translateZ) ) / perspective );
+//   currentNode.querySelector('.parallax-img').style.left = ( mm_left * Math.abs(mm_translateZ) ) / perspective - (parseInt(window.getComputedStyle(currentNode).width)/2) + 'px';
+// })
