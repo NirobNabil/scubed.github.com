f4fcf4eb229ad394068c029c556f408fa1702160
Twin_N <thetwin11100@gmail.com>
2019-02-14 04:30:06 +0600

    added script to automatically correct position of 3d elements in XY plane, made the footer.

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,41 @@
+//loads the particle js animation
 particlesJS.load('particles-js', 'settings/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
+    console.log('callback - particles.js config loaded');
 });
 
+//initializes smooth scrolling on home-container
 var Scrollbar = window.Scrollbar;
 Scrollbar.init(document.querySelector('.home-container'));
 
 
-document.querySelectorAll('.our-services .skill').forEach(function(currentNode){
-  console.log(currentNode);
-  currentNode.addEventListener('click', function(){
-    currentNode.style.height += window.getComputedStyle(currentNode.querySelector('.skill-list')).height
-  })
+//shorthand for queryselector and queryselectorall
+//2nd parameter is a boolean and if true returns array of elements from querselectorall
+function getElem(selector, array){
+    if(array){
+        return document.querySelectorAll(selector);
+    }else{
+        return document.querySelector(selector);
+    }
+}
+
+//returns a CSSStyleDeclaration object of the selector
+function getStyles(selector){
+    let elem;
+    if(typeof selector == 'object'){
+        elem = selector;
+    }else{ 
+        elem = getElem(selector);
+    }
+    console.log(elem);
+    return window.getComputedStyle(elem);
+}
+
+//applies appropriate positioning(correcting the positioning in Z axis) on the elements that have been pushed backward using translateZ
+const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const perspective = parseInt(getStyles('.home-container').perspective);                                           //perspective of the main mother element
+getElem('.parallax-img-container', true).forEach(function(currentNode, index){                                                //translateZ value of mm image
+    let mm_translateZ = new WebKitCSSMatrix(getStyles(currentNode.querySelector('img')).webkitTransform).m43;
+    let X = parseInt(currentNode.getBoundingClientRect().x) + (parseInt(getStyles(currentNode).width) / 2);                   //x coordinate of the element's middle point
+    let mm_left = X - (viewport_width/2);                                                                                     //distance of mm from the middle of the viewport
+    currentNode.querySelector('img').style.left = (( mm_left * Math.abs(mm_translateZ) ) / perspective) + 'px';
 })
-
-
-//********* failed attempts ************//
-//(left*distance)/perspective
-// const perspective = parseInt(window.getComputedStyle(document.querySelector('.home-container')).perspective);    //perspective of the main mother element
-// const mm_translateZ = new WebKitCSSMatrix(window.getComputedStyle(document.querySelector('.mm .parallax-img')).webkitTransform).m43;  //translateZ value of mm image
-// document.querySelectorAll('.mm').forEach(function(currentNode, index){
-//   let mm_left = parseInt(window.getComputedStyle(currentNode).getPropertyValue('left'));  //X coordinate fo the current mm
-//   console.log( ( mm_left * Math.abs(mm_translateZ) ) / perspective );
-//   currentNode.querySelector('.parallax-img').style.left = ( mm_left * Math.abs(mm_translateZ) ) / perspective - (parseInt(window.getComputedStyle(currentNode).width)/2) + 'px';
-// })
